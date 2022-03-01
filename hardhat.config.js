073b771e2ab3841b767ded1e4d2e2e0e07a7e7c7
Min Weng (Max) <max.m.weng@gmail.com>
2022-03-01 17:31:17 +0800

    new: initial commit.

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+// require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy");
+require("dotenv").config();
+
+// tasks
+require("./tasks/accounts");
+
+module.exports = {
+    networks: {
+        hardhat: {
+            accounts: {
+                mnemonic: "test test test test test test test test test test test junk"
+            },
+            allowUnlimitedContractSize: true
+        },
+        kovan: {
+            url: "https://kovan.infura.io/v3/" + process.env.INFURA_ID,
+            accounts: process.env.PRIVATE_KEY
+                ? [process.env.PRIVATE_KEY]
+                : {
+                      mnemonic: process.env.MNEMONIC ? process.env.MNEMONIC : process.env.MNEMONIC_TEST
+                  }
+        },
+        mainnet: {
+            url: "https://mainnet.infura.io/v3/" + process.env.INFURA_ID,
+            accounts: process.env.PRIVATE_KEY
+                ? [process.env.PRIVATE_KEY]
+                : {
+                      mnemonic: process.env.MNEMONIC ? process.env.MNEMONIC : process.env.MNEMONIC_TEST
+                  }
+        }
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.4",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 100
+                    },
+                    evmVersion: "istanbul"
+                }
+            }
+        ]
+    },
+    paths: {sources: "./proposals"},
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            4: 0
+        }
+    }
+};
---
+++ utils/index.js
@@ -0,0 +1,36 @@
+const {ethers} = require("hardhat");
+
+const waitNBlocks = async n => {
+    await Promise.all(
+        [...Array(n).keys()].map(async () => {
+            await ethers.provider.send("evm_mine");
+        })
+    );
+};
+
+const increaseTime = async seconds => {
+    await ethers.provider.send("evm_increaseTime", [seconds]);
+    await ethers.provider.send("evm_mine");
+};
+
+const encodeParameters = (types, values) => {
+    const abi = new ethers.utils.AbiCoder();
+    return abi.encode(types, values);
+};
+
+function etherMantissa(num, scale = 1e18) {
+    if (num < 0) return ethers.BigNumber.from(ethers.BigNumber.from(2).pow(256).plus(num).toFixed());
+    return ethers.BigNumber.from(ethers.BigNumber.from(num).times(scale).toFixed());
+}
+
+function etherUnsigned(num) {
+    return ethers.BigNumber.from(ethers.BigNumber.from(num).toFixed());
+}
+
+module.exports = {
+    waitNBlocks,
+    increaseTime,
+    encodeParameters,
+    etherMantissa,
+    etherUnsigned
+};
