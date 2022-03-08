e49ba1a9762e693e5585153d0fae6b7b74a469e6
Min Weng (Max) <max.m.weng@gmail.com>
2022-03-08 16:30:49 +0800

    chg: update copy.

--- proposals/003_TreasuryAdminTransfer/proposal.js
+++ proposals/003_TreasuryAdminTransfer/proposal.js
@@ -10,11 +10,32 @@ async function getProposalParams({treasuryAddress}) {
     const sigs = ["acceptAdmin()"];
     const calldatas = [ethers.utils.defaultAbiCoder.encode([], [])];
     const msg = `
-UIP-003: Transfer treasury's ownership to timelock
+UIP-003: Transfer treasury's ownership to Timelock
 
-# Proposals
+# Simple Summary
 
-- Accept the Treasury admin update (changing to Timelock)
+In short, as part of progressive decentralization, this proposal transfers ownership of the treasury from Admin key to DAO governance. In order to execute https://unioncredit.slite.com/app/docs/LKtqwvcEiQDocE, the treasury's ownership must belong to Timelock. This proposal executes the transfer of ownership.
+
+# Motivation
+
+Increasing decentralization is the primary motivation. In addition, to execute UIP 0004, setting up a UNION drip from Mainnet to Arbitrum One, treasury ownership must be transferred to Timelock from Admin key.
+
+# Specification
+
+- Prerequisites: Set the 'newAdmin' property of the Treasury contract to be the Timelock's address (0x107e3811900A93940cE8694fF9C6217Be900faAF).
+- Call the function 'acceptAdmin()' of the Treasury contract to set its 'admin' property.
+
+# Test Cases
+
+Simulation on mainnet here: https://github.com/unioncredit/union-v1-proposals/blob/uip/003-treasury-admin-transfer/proposals/003_TreasuryAdminTransfer/testProposal.js
+
+# Implementation
+
+- Call the function 'acceptAdmin()' of the Treasury contract
+
+# Security Considerations
+
+Make sure the new admin is Timelock's address. UIP 0003 and UIP 0004 could have been completed together, but as this change pertains to the treasury, and abundance of caution is beneficial to limit risk. Therefore, the changes were split up into individual proposals and will be handled one at a time.
 
 `;
 
