380831e501261a2d4e0351ff24ff32d18e7b7359
Min Weng (Max) <max.m.weng@gmail.com>
2022-06-22 20:27:52 +0700

    fix: adapter address.

--- proposals_in_review/005_AaveV3Adapter/addresses.js
+++ proposals_in_review/005_AaveV3Adapter/addresses.js
@@ -13,7 +13,7 @@ Object.keys(addresses).map(networkID => {
             addresses[networkID] = Object.assign(addresses[networkID], {
                 unionTokenAddress: "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C", //Address on Mainnet
                 assetManagerAddress: "0x7Aecd107Cb022e1DFd42cC43E9BA94C38BC83275", //Address on Arbitrum
-                adapterAddress: "0x393d7299c2caA940b777b014a094C3B2ea45ee2Bd" //Address on Arbitrum
+                adapterAddress: "0x393d7299c2caA940b777b014a094C3B2ea45ee2B" //Address on Arbitrum
             });
             break;
     }
